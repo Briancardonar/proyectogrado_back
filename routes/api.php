aeac5cefcf058eb81c186745d3b7dc4acd0d86ff
Briancardonar <bricardar2002@gmail.com>
2022-05-28 13:20:10 -0500

    first project commit

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/productos', 'index');
+    Route::post('/producto', 'store');
+    Route::get('/producto/{id}', 'show');
+    Route::put('/producto/{id}', 'update');
+    Route::delete('/producto/{id}', 'destroy');
+});
